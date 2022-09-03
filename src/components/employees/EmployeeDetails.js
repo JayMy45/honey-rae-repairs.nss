4106564806f1fbc8675ac7594a1796a667e55cd0
Jeremy Myers <echelonical@gmail.com>
2022-09-03 14:08:06 -0500

    created new employeeDetails that displays specialty email, and payRate via specified fetch and props

--- src/components/employees/EmployeeDetails.js
+++ src/components/employees/EmployeeDetails.js
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import "./Employees.css"
 
 export const EmployeeDetails = () => {
     const { employeeId } = useParams()
-    const [employee, setEmployee] = useState()
+    const [employee, setEmployee] = useState([])
 
 
     useEffect(
@@ -17,5 +18,14 @@ export const EmployeeDetails = () => {
         },
         [employeeId]
     )
-    return <></>
+    return <section className="employee">
+        <header className="employee__header">{employee?.user?.fullName}</header>
+
+        <div>Email: {employee?.user?.email}</div>
+        <div>Specialty: {employee.specialty}</div>
+        <div>Rate: {employee.rate}</div>
+
+        <footer className="employee__footer">Currently working on {employee?.employeeTickets?.length} tickets</footer>
+
+    </section>
 }
